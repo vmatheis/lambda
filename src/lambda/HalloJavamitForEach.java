994934272b370ced889668b29777203de3e74991
vmatheis <Matheis.Vanessa@sus.htl-grieskirchen.at>
2020-09-28 13:27:24 +0200

    2. Speicherung

---
+++ src/lambda/HalloJavamitForEach.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author vmatheis
+ */
+public class HalloJavamitForEach {
+    static List<String> list = new ArrayList<String>();
+    
+    public static void main(String[] args) {
+       while(true){
+           list.add("lol");
+           if(list.size() == 6){
+               break;
+           }
+       }
+        
+        for (String string : list) {
+            System.out.println(string);
+        }
+        System.out.println("Lambda:");
+        list.forEach(System.out::println);
+    }
+    
+    
+}
